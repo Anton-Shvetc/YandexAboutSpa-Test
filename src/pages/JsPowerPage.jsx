c2bae0c147ba9ee2a225210d75ececf9c61cb56d
Anton <antonshvetswork@yandex.ru>
2023-08-18 01:22:18 +0300

    add video about css future

--- src/pages/JsPowerPage.jsx
+++ src/pages/JsPowerPage.jsx
@@ -6,9 +6,20 @@ function JsPowerPage() {
     <>
       <Box sx={{ maxWidth: "90%", margin: "auto" }}>
         <Typography variant="h2"> Сила JavaScript </Typography>
-        <Typography>
-          Это дополнительная страница, которая поможет узнать на что способен
-          JavaScript
+        <Typography sx={{ fontSize: "18px", mb: "10px" }}>
+          Это достаточно простая активность, которая верусится с очень давних
+          времен, но я думаю, что ее обязательно нужно попробовать, а именно,
+          попробовать угадать, что выведет данный код?
+        </Typography>
+        <Box sx={{mb:"10px"}}>
+          <Typography> console.log(Array(16).join("wat")); </Typography>
+          <Typography>console.log(Array(16).join("wat" +1)); </Typography>
+          <Typography>
+            console.log(Array(16).join("wat" - 1) + " Batman!");{" "}
+          </Typography>
+        </Box>
+        <Typography sx={{ fontSize: "18px", mb: "10px" }}>
+         Возможно не силу, но особенности языка точно покажет
         </Typography>
       </Box>
     </>
--- src/pages/OptionalInfoPage.jsx
+++ src/pages/OptionalInfoPage.jsx
@@ -3,6 +3,7 @@ import useToken from "../components/useToken";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
+import video from "../assets/video/aboutCss.mp4";
 
 function OptionalInfoPage() {
   const { token, setToken } = useToken();
@@ -10,7 +11,7 @@ function OptionalInfoPage() {
     return (
       <>
         <Box sx={{ maxWidth: "90%", margin: "auto" }}>
-          <Typography variant="h2">Полезные ссылки</Typography>
+          <Typography variant="h2">Полезные ссылки:</Typography>
           <Typography variant="h4">
             Это страница с дополнительной информацией
           </Typography>
@@ -27,10 +28,22 @@ function OptionalInfoPage() {
   return (
     <Box sx={{ maxWidth: "90%", margin: "auto" }}>
       <Typography variant="h2">Полезные ссылки</Typography>
-      <Typography variant="h4">Ссылка на репозиторий:</Typography>
-      <Link href="https://github.com/Anton-Shvetc/YandexAboutSpa-Test">
-        Перейти
-      </Link>
+      <Box sx={{ mb: "20px" }}>
+        <Typography variant="h4" sx={{ mb: "10px" }}>
+          Ссылка на репозиторий:
+        </Typography>
+        <Link href="https://github.com/Anton-Shvetc/YandexAboutSpa-Test">
+          Перейти
+        </Link>
+      </Box>
+
+      <Box></Box>
+      <Typography variant="h4" sx={{ mb: "10px" }}>
+        Немного о классной фишке в CSS:
+      </Typography>
+      <video style={{ maxWidth: "500px" }} src={video} controls>
+        <Typography> Ваш браузер не поддерживает HTML5 видео.</Typography>
+      </video>
     </Box>
   );
 }
